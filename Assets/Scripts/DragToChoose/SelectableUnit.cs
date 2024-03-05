d615c3fb72d278ecdacf7bea9d04ff00be075674
hoangtien47 <hoangtien472003@gmail.com>
2024-03-05 18:13:17 +0700

    drag and select

---
+++ Assets/Scripts/DragToChoose/SelectableUnit.cs
@@ -0,0 +1,95 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class SelectableUnit : MonoBehaviour
+{
+    private NavMeshAgent Agent;
+    [SerializeField]
+    private SpriteRenderer SelectionSprite;
+
+    private void Awake()
+    {
+        SelectionManager.Instance.AvailableUnits.Add(this);
+        Agent = GetComponent<NavMeshAgent>();
+        SelectionSprite = GetComponent<SpriteRenderer>();
+    }
+    private void Start()
+    {
+        Agent.updateRotation = false;
+        Agent.updateUpAxis = false;
+        SelectionSprite.color = new Color(0.7f, 0.7f, 0.7f);
+    }
+    public void MoveTo(Vector3 Position)
+    {
+        Debug.Log(Position);
+        Agent.SetDestination(Position);
+    }
+
+    public void OnSelected()
+    {
+        //SelectionSprite.gameObject.SetActive(true);
+        SelectionSprite.color = new Color(1f, 1f, 1f);
+
+    }
+
+    public void OnDeselected()
+    {
+        //SelectionSprite.gameObject.SetActive(false);
+        SelectionSprite.color = new Color(0.7f, 0.7f, 0.7f);
+    }
+}
+
+
+public class SelectionManager
+{
+    private static SelectionManager _instance;
+    public static SelectionManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new SelectionManager();
+            }
+
+            return _instance;
+        }
+        private set
+        {
+            _instance = value;
+        }
+    }
+
+    public HashSet<SelectableUnit> SelectedUnits = new HashSet<SelectableUnit>();
+    public List<SelectableUnit> AvailableUnits = new List<SelectableUnit>();
+
+    private SelectionManager() { }
+
+    public void Select(SelectableUnit Unit)
+    {
+        SelectedUnits.Add(Unit);
+        Unit.OnSelected();
+    }
+
+    public void Deselect(SelectableUnit Unit)
+    {
+        Unit.OnDeselected();
+        SelectedUnits.Remove(Unit);
+    }
+
+    public void DeselectAll()
+    {
+        foreach (SelectableUnit unit in SelectedUnits)
+        {
+            unit.OnDeselected();
+        }
+        SelectedUnits.Clear();
+    }
+
+    public bool IsSelected(SelectableUnit Unit)
+    {
+        return SelectedUnits.Contains(Unit);
+    }
+}
