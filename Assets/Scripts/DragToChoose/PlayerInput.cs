f3ad0411042fa75ce2b4e090d1d785343a8ad214
hoangtien47 <hoangtien472003@gmail.com>
2024-03-08 16:23:03 +0700

    pawn ai

--- Assets/Scripts/DragToChoose/PlayerInput.cs
+++ Assets/Scripts/DragToChoose/PlayerInput.cs
@@ -13,6 +13,9 @@ public class PlayerInput : MonoBehaviour
     [SerializeField]
     private LayerMask FloorLayers;
     [SerializeField]
+    private LayerMask WorkLayers;
+
+    [SerializeField]
     private float DragDelay = 0.1f;
 
     private float MouseDownTime;
@@ -49,6 +52,7 @@ public class PlayerInput : MonoBehaviour
                     unit.MoveTo(Hit.point);
                 }
             }*/
+
         }
     }
 
