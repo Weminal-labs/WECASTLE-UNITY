326db14b45fe1b9e9dba88a1bbc8f989f9e268e3
hoangtien47 <hoangtien472003@gmail.com>
2024-04-02 20:11:38 +0700

    pawn behav

--- Assets/MonoBehaviourTree/Source/Runtime/Nodes/MakeDamage.cs
+++ Assets/MonoBehaviourTree/Source/Runtime/Nodes/MakeDamage.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class MakeDamage : MonoBehaviour
+namespace MBT
 {
-    // Start is called before the first frame update
-    void Start()
+    [AddComponentMenu("")]
+    [MBTNode(name = "Tasks/Make Damage")]
+    public class MakeDamage : Leaf
     {
-        
-    }
+        public GameObjectReference target;
+        public GameObjectReference self;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+        public override NodeResult Execute()
+        {
+            if (target == null || self == null)
+            {
+                Debug.LogError("Prefap reference is not set in Set Prefap node.");
+                return NodeResult.failure;
+            }
+            
+/*
+            MobStatus mobStatus = self.Value.GetComponent<MobStatus>();
+
+            NewBehaviourScript newBehaviourScript = target.Value.GetComponent<NewBehaviourScript>();
+
+            newBehaviourScript.TakeDmg(mobStatus.getDamage());*/
+            // 
+
+            return NodeResult.success;
+        }
+
+        public override bool IsValid()
+        {
+            return target != null && self != null;
+        }
     }
 }
