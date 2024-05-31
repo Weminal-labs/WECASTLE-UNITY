c973abee1b78558f22aab7e7f3b7330ab7208836
orp1205 <98261194+orp1205@users.noreply.github.com>
2024-05-31 20:37:04 +0700

    change map

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
